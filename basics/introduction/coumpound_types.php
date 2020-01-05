2157d16ac29d4d0b5f0b69aa793398a2f6e98baf
Anderson Lucas <a.lucassilvadeoliveira@gmail.com>
2020-01-04 23:06:10 -0300

    Add compound_types

--- basics/introduction/coumpound_types.php
+++ basics/introduction/coumpound_types.php
@@ -28,3 +28,51 @@
     var_dump($scalarClass->scalar);
     $scalarClass->xd = 2; //dynamic property set.
     var_dump($scalarClass);
+?>
+<hr>
+<h2>Callables/Callback</h2>
+
+<?php 
+    /**
+     *CALLBACK / CALLABLE
+     */
+    function foo($msg= ""){
+        echo "bar <br>";
+    }
+
+    /**
+     * CLOSURE
+     */
+    $foox = function ($msg) {
+        echo 'foo2'.$msg . "<br>";
+    }; 
+
+    class Bar 
+    {
+        public function Method()
+        {
+            echo "Bar method!";
+        }
+    }
+
+    class Foo extends Bar
+    {
+        public function bar($msg = "")
+        {
+            echo "msg = $msg <br>";
+        }
+    }
+
+    call_user_func('foo'); //access callback with the name string
+    call_user_func('Foo'); // this is case insenstive
+    
+    // call_user_func('foo2'); //not work because the closure it's a variable
+    
+    call_user_func(['Foo', 'bar']); //it's work 
+    call_user_func_array(['Foo', 'bar'], ['myMessage']); //with params
+    call_user_func($foox, 'my message');
+    call_user_func_array($foox, ['myclosure!']);
+    
+    /**
+     * Execute the parent method
+     */
