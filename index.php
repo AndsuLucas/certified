58e1532a49c7ab4e36353216708481f5c09a59b7
Anderson Lucas <a.lucassilvadeoliveira@gmail.com>
2021-02-24 20:58:13 -0300

    add error handling and change index

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 
-echo "hello world";
-opcache_reset();
-var_dump(opcache_get_status());
 
-$var = 48;
-++ $var;
+echo 8 >> 4;
+echo 4 << 7;
 
-echo $var;
+echo "<hr>";
+
+$a = 2;
+$a <<= 2;
+// echo $a;
+echo "<hr>";
+echo ~5;
+echo "<hr>";
+
+echo 5 & 80;
+echo "<hr>";
+
+echo 4 & 6;
+echo "<hr>";
+echo 16 & 48;
+echo "<hr>";	
+echo 28 | 34;
+echo "<hr>";
+echo 3 ^ 2;
+echo "<hr>";
+echo 88 ^ 47;
